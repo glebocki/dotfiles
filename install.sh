98945882f339fdcfcdb925ce6ab2043ef3030557
Przemysław Głębocki <glebocki.przemyslaw@gmail.com>
2015-10-13 14:38:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ echo "This script will instal software for Glebocki machine"
 echo "Update"
 sudo apt-get update
 
-echo "Install Git, Vim, Java, Yo"
+echo "Install Tree"
+sudo apt-get install tree -y
+
+echo "Install Git, Vim"
 sudo apt-get install git vim java yo -y
 
+echo "Yeoman(Yo), gulp, bower"
+npm install -g yo bower grunt-cli gulp -y
