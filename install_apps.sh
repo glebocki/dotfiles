8492d2773b81e9c3d4f1f8ad051426c7ded6f18c
Przemysław Głębocki <glebocki.przemyslaw@gmail.com>
2016-07-12 17:35:53 +0200

    added cppreference-doc-en-html and vim

--- install_apps.sh
+++ install_apps.sh
@@ -2,9 +2,13 @@
 
 # Most important apps
 sudo apt-get install git
+sudo apt-get install vim
 
 # Oracle JDK/JRE 8 - ppa from webupd8team
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java8-installer
 sudo apt-get install oracle-java8-set-default
+
+# Useful packages
+sudo apt-get install cppreference-doc-en-html
