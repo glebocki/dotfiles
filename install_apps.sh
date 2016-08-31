95ff4738c220e607b45b9a2fe4fe76b90387d293
Przemysław Głębocki <glebocki.przemyslaw@gmail.com>
2016-08-31 22:22:07 +0200

    add Sass

--- install_apps.sh
+++ install_apps.sh
@@ -4,6 +4,12 @@
 sudo apt-get install git
 sudo apt-get install vim
 
+# Install languages
+sudo apt-get install gcc g++ ruby -y
+
+# Install Sass
+sudo su -c "gem install sass"
+
 # Oracle JDK/JRE 8 - ppa from webupd8team
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
